a7d496e0c907d9db30420af049857dfb9652a14b
Vladimir Ageev <vladimir.ageev@me.com>
2016-10-17 19:57:08 +0300

    Initial Commit

---
+++ Weather/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Weather
+//
+//  Created by Vladimir on 17.10.16.
+//  Copyright © 2016 Vladimir Ageev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
