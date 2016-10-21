36063ad60fd30a514f9e258f22953b8c59fdb6cd
Vladimir Ageev <vladimir.ageev@me.com>
2016-10-21 11:20:24 +0300

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+target 'Weather' do
+use_frameworks!
+pod 'SwiftyJSON'
+pod 'Alamofire', '~> 4.0'
+end
---
+++ Weather/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Vladimir on 17.10.16.
+//  Copyright © 2016 Vladimir Ageev. All rights reserved.
+//
+
+import Foundation
+
+class Constants{
+   let apiKey = "31e09cd3554be4412395f27921a50795"
+}
